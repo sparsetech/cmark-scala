4432ae1c89163f06ef41e6378044a235f539858d
Darcy Shen <sadhen@zoho.com>
2019-03-13 22:11:01 +0800

    Finish CMarkSuite

--- tests/src/test/scala/cmark/CMarkSuite.scala
+++ tests/src/test/scala/cmark/CMarkSuite.scala
@@ -3,61 +3,57 @@ package cmark
 import scalanative.native._
 
 object CMarkSuite extends tests.Suite {
-  test("") {
-    val msg: CString = c"Hello, world!"
-    stdio.printf(msg)
+  test("Print version string of cmark") {
+    stdio.printf(cmark.versionString())
   }
-    //"Example" - {
-      // assert(fromCString(cmark.versionString()) == "0.1")
-      // var level = -1
-      // def onNode(eventType: EventType, node: Ptr[Node]): Unit = {
-      //   eventType match {
-      //     case EventType.Enter => level += 1
-      //     case EventType.Exit  => level -= 1
-      //   }
 
-      //   val levelStr = "  " * level
-      //   val startLine = Node.getStartLine(node)
-      //   val endLine   = Node.getEndLine(node)
+  test("Example") {
+    var level = -1
+    def onNode(eventType: EventType, node: Ptr[Node]): Unit = {
+      eventType match {
+        case EventType.Enter => level += 1
+        case EventType.Exit  => level -= 1
+      }
 
-      //   Node.getType(node) match {
-      //     case NodeType.Text =>
-      //       val text = fromCString(Node.getLiteral(node))
-      //       println(s"${levelStr}text node @ line $startLine-$endLine: $text")
+      val levelStr = "  " * level
+      val startLine = Node.getStartLine(node)
+      val endLine   = Node.getEndLine(node)
 
-      //     case _ =>
-      //       val nodeTypeStr = fromCString(Node.getTypeString(node))
-      //       println(s"$levelStr$nodeTypeStr node @ $startLine-$endLine")
-      //   }
-      // }
+      Node.getType(node) match {
+        case NodeType.Text =>
+          val text = fromCString(Node.getLiteral(node))
+          println(s"${levelStr}text node @ line $startLine-$endLine: $text")
 
-      // val test =
-      //   """# Chapter
-      //     |## Section
-      //     |### Sub-section
-      //     |
-      //     |Hello World from *cmark-scala*!
-      //   """.stripMargin
+        case _ =>
+          val nodeTypeStr = fromCString(Node.getTypeString(node))
+          println(s"$levelStr$nodeTypeStr node @ $startLine-$endLine")
+      }
+    }
 
-      // println("cmark version: " + fromCString(cmark.versionString()))
-      // println()
+    val test =
+      """# Chapter
+        |## Section
+        |### Sub-section
+        |
+        |Hello World from *cmark-scala*!
+      """.stripMargin
 
-      // Zone { implicit z =>
-      //   val docNode = Parser.parseDocument(
-      //     toCString(test), test.length, Options.SourcePosition)
-      //   val iter = Iter.create(docNode)
-      //   var evType = Iter.next(iter)
-      //   while (evType != EventType.Done) {
-      //     onNode(evType, Iter.getNode(iter))
-      //     evType = Iter.next(iter)
-      //   }
-      //   Iter.free(iter)
+    Zone { implicit z =>
+      val docNode = Parser.parseDocument(
+        toCString(test), test.length, Options.SourcePosition)
+      val iter = Iter.create(docNode)
+      var evType = Iter.next(iter)
+      while (evType != EventType.Done) {
+        onNode(evType, Iter.getNode(iter))
+        evType = Iter.next(iter)
+      }
+      Iter.free(iter)
 
-      //   val html = fromCString(Render.html(docNode, Options.Default))
-      //   Node.free(docNode)
+      val html = fromCString(Render.html(docNode, Options.Default))
+      Node.free(docNode)
 
-      //   println()
-      //   println(html)
-      // }
-    //}
+      println()
+      println(html)
+    }
+  }
 }
