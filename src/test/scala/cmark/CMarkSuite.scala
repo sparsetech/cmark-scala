84adcc96edd6b9f9e8eb998d8fcc66b29ae72160
Darcy Shen <sadhen@zoho.com>
2019-03-15 00:39:14 +0800

    Use minitest

---
+++ src/test/scala/cmark/CMarkSuite.scala
@@ -0,0 +1,61 @@
+package cmark
+
+import minitest._
+import scalanative.native._
+
+object CMarkSuite extends SimpleTestSuite {
+  test("Print version string of cmark") {
+    println("cmark version: " + fromCString(cmark.versionString()))
+  }
+
+  test("Example") {
+    var level = -1
+
+    def onNode(eventType: EventType, node: Ptr[Node]): Unit = {
+      eventType match {
+        case EventType.Enter => level += 1
+        case EventType.Exit => level -= 1
+      }
+
+      val levelStr = "  " * level
+      val startLine = Node.getStartLine(node)
+      val endLine = Node.getEndLine(node)
+
+      Node.getType(node) match {
+        case NodeType.Text =>
+          val text = fromCString(Node.getLiteral(node))
+          println(s"${levelStr}text node @ line $startLine-$endLine: $text")
+
+        case _ =>
+          val nodeTypeStr = fromCString(Node.getTypeString(node))
+          println(s"$levelStr$nodeTypeStr node @ $startLine-$endLine")
+      }
+    }
+
+    val test =
+      """# Chapter
+        |## Section
+        |### Sub-section
+        |
+        |Hello World from *cmark-scala*!
+      """.stripMargin
+
+    Zone { implicit z =>
+      val docNode = Parser.parseDocument(
+        toCString(test), test.length, Options.SourcePosition)
+      val iter = Iter.create(docNode)
+      var evType = Iter.next(iter)
+      while (evType != EventType.Done) {
+        onNode(evType, Iter.getNode(iter))
+        evType = Iter.next(iter)
+      }
+      Iter.free(iter)
+
+      val html = fromCString(Render.html(docNode, Options.Default))
+      Node.free(docNode)
+
+      println()
+      println(html)
+    }
+  }
+}
